76275ec604a25d6749b27a52200190009f2a3ab5
franz <fkrekeler@gmail.com>
2018-10-17 12:53:12 +0200

    96 percent complete

--- _site/static/sfc/src/main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#sfc',
-  render: h => h(App)
-})
--- static/ts/src/main.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#ts',
-  render: h => h(App)
-})
